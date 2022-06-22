06f31537c3cdb838dcf08587adc2f717172049b2
p55643na <nor.ahmadfazrulshahril@student.manchester.ac.uk>
2022-06-22 08:23:29 +0100

    zones classes done

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/Zone.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/Zone.java
@@ -7,64 +7,25 @@ abstract class Zone {
     protected float x_max;
     protected float y_max;
     protected float z_max;
+    protected float length;
+    protected float width;
+    protected float height;
 
-    public Zone(float x_min, float y_min, float z_min, float x_max, float y_max, float z_max){
+    public Zone(float x_min, float y_min, float z_min, float x_max, float y_max, float z_max, float length, float width, float height){
         this.x_min = x_min;
         this.y_min = y_min;
         this.z_min = z_min;
         this.x_max = x_max;
         this.y_max = y_max;
         this.z_max = z_max;
+        this.length = length;
+        this.width = width;
+        this.height = height;
     }
 
-    float r1 =
-    // r2 =
-    // l1 =
-    // l2 =
-    // t1 =
-    // t2 =
-    // b1 =
-    // b2 =
+    public boolean contains(float x, float y, float z){
 
-    // calculate r1 ... b2 with inputs
-    //
-
-    public boolean checkPoint(){
-        //corner collision check
-        if(r1>l2 && t2>b1 && t1>t2 && b1>b2){
-            System.out.println("Corner Collision check 1");
-            return true;
-        }
-        if(r2>l1 && t2>b1 && t1>t2 && b1>b2){
-            System.out.println("Corner Collision check 2");
-            return true;
-        }
-        if(r2>l1 && t1>b2 && t2>t1 && b2>b1){
-            System.out.println("Corner Collision check 3");
-            return true;
-
-        }
-        if(r1>l2 && t1>b2 && t2>t1 && b2>b1){
-            System.out.println("Corner Collision check 4");
-            return true;
-        }
-
-        //middle collision check
-        if(l1>l2 && r1<r2 && t1<t2 && b1<b2){
-            System.out.println("middle collision check 1");
-            return true;
-        }
-        if(l1>l2 && r1<r2 && t1>t2 && b1>b2){
-            System.out.println("middle Collision check 2");
-            return true;
-        }
-        if(l1<l2 && r1<r2 && t1<t2 && b1>b2){
-            System.out.println("middle Collision check 3");
-            return true;
-        }
-        if(l1>l2 && r1>r2 && t1<t2 && b1>b2){
-            return true;
-        }
+        if(x >= x_min && x<= x_max && y >= y_min && y <= y_max && z >+ z_min && z <= z_max) return true;
 
         return false;
     }
