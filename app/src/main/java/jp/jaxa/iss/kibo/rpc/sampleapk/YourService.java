79f06a9210cfc9d785e7423f348e5dc401eba8b5
p55643na <nor.ahmadfazrulshahril@student.manchester.ac.uk>
2022-06-26 16:14:42 +0100

    barebones of moving bee towards aruco marker

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -1,6 +1,6 @@
 package jp.jaxa.iss.kibo.rpc.sampleapk;
 
-import android.graphics.Bitmap;
+import android.os.Looper;
 import android.util.Log;
 
 import gov.nasa.arc.astrobee.Kinematics;
@@ -10,7 +10,6 @@ import gov.nasa.arc.astrobee.Result;
 import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
 
-import org.opencv.android.Utils;
 import org.opencv.aruco.Board;
 import org.opencv.aruco.DetectorParameters;
 import org.opencv.aruco.Dictionary;
@@ -37,8 +36,6 @@ public class YourService extends KiboRpcService {
     private final KeepInZone KIZ01 = new KeepInZone(10.3f, -10.2f, 4.32f, 11.55f, -6.4f, 5.57f);
     private final KeepInZone KIZ02 = new KeepInZone(9.5f, -10.5f, 4.02f, 10.5f, -9.6f, 4.8f);
 
-    private final int LOOP_MAX = 5;
-
 
     @Override
     protected void runPlan1(){
@@ -51,6 +48,7 @@ public class YourService extends KiboRpcService {
 
         // POINT 1
         Point point = new Point(10.71f, -7.9f, 4.48f);
+        // Point point = new Point(10.3f, -7.3f, 4.48f); tested with this, but failed
         Quaternion quaternion = new Quaternion(0f, 0.707f, 0f, 0.707f);
         moveBee(point, quaternion, 1);
 
@@ -87,6 +85,7 @@ public class YourService extends KiboRpcService {
 
         // POINT 4 : move forward towards 'POINT 2 - as given in rulebook'
         point = new Point(11.27460f, -9.92284f, 5.29881f);
+        // point = new Point(11.4f, -9.92284f, 5.29881f); tested with this but failed?
         quaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
         moveBee(point, quaternion, 4);
 
@@ -127,11 +126,14 @@ public class YourService extends KiboRpcService {
         Log.i(TAG, "make dictionary");
         Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
         List<Mat> corners = new ArrayList<Mat>();
-        Mat ids = new Mat(10, 10, 10, new Scalar( 150, 150, 150 ));
+
+        Mat ids = new Mat(1, 4, 1, new Scalar( 0, 150, 250 ));
+
         Mat cameraMatrix = new Mat();
         Mat distCoeffs = new Mat();
         List<Mat> rvecs = new ArrayList<Mat>();
         List<Mat> tvecs = new ArrayList<Mat>();
+        List objPoints = new ArrayList();
         Mat counter = new Mat();
         Size imageSize = new Size(5, 5);
 
@@ -142,52 +144,35 @@ public class YourService extends KiboRpcService {
         // TARGET 1 image processing
         Log.i(TAG, "TARGET 1 image processing");
         Aruco.detectMarkers(imageMatTarget1, dictionary, corners, ids, detectorParameters);
-        Aruco.drawDetectedMarkers(imageMatTarget1, corners, ids, new Scalar( 150, 150, 150 ));
-
-        Board board = Board.create(corners, dictionary, ids);
-        Aruco.calibrateCameraAruco(corners, ids, counter, board, imageSize, cameraMatrix, distCoeffs, rvecs, tvecs);
-        Aruco.estimatePoseSingleMarkers(corners, 5, cameraMatrix, distCoeffs, rvecs.get(0), tvecs.get(0));
+        Aruco.drawDetectedMarkers(imageMatTarget1, corners, ids, new Scalar( 0, 150, 250 ));
 
         api.saveMatImage(imageMatTarget1, "processedNearTarget1.png");
+        Log.i("image length", "height: " + imageMatTarget1.height() + ", width: " + imageMatTarget1.width());
 
-        // saving images
-//        for (int i=0;i<corners.size();i++) {
-//            Bitmap image = null;
-//            Utils.matToBitmap(corners.get(i), image);
-//            api.saveBitmapImage(image, "corners" + i + ".png");
-//            Log.i(TAG, "saved corner image");
-//        }
-//        if (ids.width()!=0 && ids.height()!=0) {
-//            Bitmap image = null;
-//            Utils.matToBitmap(ids, image);
-//            api.saveBitmapImage(image, "ids_TARGET2.png");
-//            Log.i(TAG, "saved ids image");
-//        }
+        moveCloserToArucoMarker(inspectCorners(corners));
 
+        Log.i(TAG, "make board");
+        // Board board = Board.create(objPoints, dictionary, ids);
+        Log.i(TAG, "made board");
+        // Aruco.calibrateCameraAruco(corners, ids, counter, board, imageSize, cameraMatrix, distCoeffs, rvecs, tvecs);
+        // Aruco.estimatePoseSingleMarkers(corners, 0.05f, cameraMatrix, distCoeffs, rvecs.get(0), tvecs.get(0));
 
+        corners.clear();
 
         // TARGET 2 image processing
         Log.i(TAG, "TARGET 2 image processing");
         Aruco.detectMarkers(imageMatTarget2, dictionary, corners, ids, detectorParameters);
-        Aruco.drawDetectedMarkers(imageMatTarget2, corners, ids, new Scalar( 150, 150, 150 ));
+        Aruco.drawDetectedMarkers(imageMatTarget2, corners, ids, new Scalar( 0, 150, 250 ));
 
         api.saveMatImage(imageMatTarget2, "processedNearTarget2.png");
+        Log.i("image length", "height: " + imageMatTarget2.height() + ", width: " + imageMatTarget2.width());
 
-        // saving images
-//        for (int i=0;i<corners.size();i++) {
-//            Bitmap image = null;
-//            Utils.matToBitmap(corners.get(i), image);
-//            api.saveBitmapImage(image, "corners" + i + ".png");
-//            Log.i(TAG, "saved corner image");
-//        }
-//        if (ids.width()!=0 && ids.height()!=0) {
-//            Bitmap image = null;
-//            Utils.matToBitmap(ids, image);
-//            api.saveBitmapImage(image, "ids_TARGET2.png");
-//            Log.i(TAG, "saved ids image");
-//        }
+        moveCloserToArucoMarker(inspectCorners(corners));
 
-        // Kinematics kinematics =  api.getRobotKinematics();
+        //Kinematics kinematics =  api.getRobotKinematics();
+        // kinematics.getOrientation();
+        // kinematics.getPosition();
+
 
         // send mission completion
         api.reportMissionCompletion();
@@ -213,6 +198,8 @@ public class YourService extends KiboRpcService {
 
     private void moveBee(Point point, Quaternion quaternion, int pointNumber){
 
+        final int LOOP_MAX = 5;
+
         if (checksForKOZ(point)) Log.i(TAG, "point " + pointNumber + " is NOT in KOZ");
         else Log.e(TAG, "point " + pointNumber + " is in KOZ");
         if (checksForKIZ(point)) Log.i(TAG, "point " + pointNumber + " is in KIZ");
@@ -229,6 +216,67 @@ public class YourService extends KiboRpcService {
         }
         if (result.hasSucceeded()) Log.i(TAG, "successfully moved to point " + pointNumber);
         else Log.e(TAG, "failed to move to point " + pointNumber);
+    }
+
+    private double[] inspectCorners(List<Mat> corners) {
+
+        Size size;
+        double[] topright = {0,0};
+        double[] topleft = {0,0};
+        double[] bottomleft = {0,0};
+        double[] bottomright = {0,0};
+        final int x_coords = 0;
+        final int y_coords = 1;
+
+        for (int corner=0;corner<corners.size();corner++) {
+            size = corners.get(corner).size();
+            for (int j = 0; j < size.width; j++) {
+                if (corner == 0 && j == 0) bottomleft = corners.get(corner).get(0, j);
+                if (corner == 1 && j == 2) bottomright = corners.get(corner).get(0, j);
+                if (corner == 2 && j == 0) topleft = corners.get(corner).get(0, j);
+                if (corner == 3 && j == 2) topright = corners.get(corner).get(0, j);
+            }
+        }
+
+        double aruco_middle_x = (bottomleft[x_coords] + bottomright[x_coords] + topleft[x_coords] + topright[x_coords])/4;
+        double aruco_middle_y = (bottomleft[y_coords] + bottomright[y_coords] + topleft[y_coords] + topright[y_coords])/4;
+
+        double[] aruco_middle = {aruco_middle_x, aruco_middle_y};
+
+        return aruco_middle;
+    }
+
+    private void moveCloserToArucoMarker(double[] aruco_middle) {
+
+        final int LOOP_MAX = 10;
+        final double middle_x = 1280/2;
+        final double middle_y = 960/2;
+        int counter = 0;
+
+        double aruco_middle_x = aruco_middle[0];
+        double aruco_middle_y = aruco_middle[1];
+
+        double x_difference = middle_x - aruco_middle_x;
+        double y_difference = middle_y - aruco_middle_y;
+
+        while (counter < LOOP_MAX) {
+            if (x_difference > 30) {
+                moveBee(); // move to right
+            }
+            if (x_difference < -30) {
+                moveBee(); // move to left
+            }
+        }
+
+        while (counter < LOOP_MAX) {
+            if (y_difference > 30) {
+                moveBee(); // move to right
+            }
+            if (y_difference < -30) {
+                moveBee(); // move to left
+            }
+        }
+
     }
 
 }
